0a447e56b7589f7c4da9d9e295b05ea589ffff67
rinodung <tiendungdong@gmail.com>
2021-04-26 16:23:12 +0700

    return admin page if index

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,15 +8,20 @@ class PageController extends Controller
 {
     public function index($slug, $subs = null)
     {
-        $page = Page::findBySlug($slug);
+        if($slug!=null) {
+            $page = Page::findBySlug($slug);
 
-        if (!$page) {
-            abort(404, 'Please go back to our <a href="' . url('') . '">homepage</a>.');
+            if (!$page) {
+                abort(404, 'Please go back to our <a href="' . url('') . '">homepage</a>.');
+            }
+
+            $this->data['title'] = $page->title;
+            $this->data['page'] = $page->withFakes();
+
+            return view('pages.' . $page->template, $this->data);
+        } else {
+            return redirect(backpack_url('dashboard'));
         }
 
-        $this->data['title'] = $page->title;
-        $this->data['page'] = $page->withFakes();
-
-        return view('pages.' . $page->template, $this->data);
     }
 }
